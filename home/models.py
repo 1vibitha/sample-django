7ccfc2b0dd30554bac92a209839074e34541f29b
vibitha <vibithavijayan6@gmail.com>
2024-08-06 09:50:56 +0530

    model deparment created

--- home/models.py
+++ home/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Department(models.model):
+    dept_name=models.CharField(max_length=100)
+    dept_description=models.TextField()
+    
